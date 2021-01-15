a381c6bb3eb428f0d4679986e3413b677c3ba184
unknown <jessicakazmirowski2179@gmail.com>
2021-01-15 13:47:30 -0300

    Desenvolvendo uma Progressive Web Application com React para mapear os dados do COVID19

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # covid19-dio
+
 Desenvolvendo uma Progressive Web Application com React para mapear os dados do COVID19 pelo mundo
---
+++ src/containers/Main/components/Card.jsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react'
+// eslint-disable-next-line no-unused-vars
+import PropTypes from 'prop-types'
+import { Card as CardUI } from '../../../components'
+import {
+  LabelStyled,
+  ValueStyled,
+  CardContentStyled
+} from './style'
+
+function Card({ value, label, color }) {
+  return (
+    <CardUI>
+      <CardContentStyled color={color}>
+        <ValueStyled>{value}</ValueStyled>
+        <LabelStyled>{label}</LabelStyled>
+      </CardContentStyled>
+    </CardUI>
+  )
+}
+
+export default memo(Card)
